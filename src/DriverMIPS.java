46a4e323b5a778a337de283d95e42f14d4c5ec78
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-01-22 20:12:59 -0600

    Update DriverMIPS.java
    Se modificó el driver para adaptarlo mejor al código implementado.

--- src/DriverMIPS.java
+++ src/DriverMIPS.java
@@ -12,7 +12,7 @@ public class DriverMIPS {
 
         //Instanciar la clase Scanner
         Scanner scan = new Scanner(System.in);
-        Radio radio = new Radio();
+        RadioMIPS radio = new RadioMIPS();
 
         //inicio del menú
         boolean verificador = false;
@@ -20,20 +20,10 @@ public class DriverMIPS {
         while (verificador != true){
             try {
 
-                if(radio.isON() == true){
-                    if(radio.frecuencia == true){
-                        System.out.println("\nEMISORA ACTUAL: " + radio.emisoraAM + " AM\n");
-                    } else if(radio.frecuencia == false){
-                        System.out.println("\nEMISORA ACTUAL: " + radio.emisoraFM + " FM\n");
-                    }
-                } else {
-                    System.out.println("\nNO SIGNAL\n");
-                }
-
                 System.out.println("Escriba el numero de la opcion que desea utilizar");
                 System.out.println("1. Encender");
                 System.out.println("2. Incrementar emisora");
-                System.out.println("3. Cambiar de emisora (AM/FM)");
+                System.out.println("3. Cambiar de frecuencia (AM/FM)");
                 System.out.println("4. Elegir Emisora de favoritas");
                 System.out.println("5. Asignar Emisora a favoritas");
                 System.out.println("6. Apagar");
@@ -44,21 +34,19 @@ public class DriverMIPS {
                     //Encender la radio
                     if (!radio.isON()) {
                         radio.encender();
-                        System.out.println("\nSe ha encendido la radio");
                     }
                     else {
                         System.out.println("La radio ya estaba encendida...");
                     }
                 }
                 else if (opcion == 2){
-					//Incrementar de emisión
+					//Incrementar de emisora
                     radio.incrementar();
                 }
                 else if (opcion == 3){
                     //Cambiar de frecuencia
                     if (radio.isON()) {
                         radio.frecuencia();
-                        System.out.println("Se ha cambiado de frecuencia\n");
                     }
                     else {
                         System.out.println("Necesita encender la radio primero para realizar esta accion");
@@ -68,69 +56,27 @@ public class DriverMIPS {
                     //elegir emisora de favoritas
 
                     if(radio.isON()){
-                        int boton = 0;
-                        System.out.println("\nEscoge un boton del 1 al 12\n");
-
-                        boolean correcto = false;
-
-                        while(!correcto){ //Seguirá funcionando hasta que se escoja una opción válida.
-                            try{
-                                boton = scan.nextInt();
-
-                                if (boton > 12 || boton < 1){ //Verifica que la opción sea válida.
-                                    System.out.println("\nOPCION INVALIDA.\n");
-                                }  else {
-                                    radio.emisora(boton - 1);
-                                    correcto = true;
-                                }
-
-                            } catch (InputMismatchException e){ //Si el usuario ingresa un valor no numerico, se ejecutará esto.
-                                System.out.println("\nPor favor ingrese una cantidad numerica.\n");
-                                scan.next();
-                            }
-                        }
+                        radio.emisora(0);
                     } else {
                         System.out.println("Necesita encender la radio primero para realizar esta accion");
                     }
 
                 }
                 else if (opcion == 5){
-                    //Agregar estación a favoritas
+                    //Agregar emisora a la lista de favoritas
 
                     if(radio.isON()){
-                        int boton = 0;
-                        System.out.println("\nEscoge un boton del 1 al 12\n");
-
-                        boolean correcto = false;
-
-                        while(!correcto){ //Seguirá funcionando hasta que se escoja una opción válida.
-                            try{
-                                boton = scan.nextInt();
-
-                                if (boton > 12 || boton < 1){ //Verifica que la opción sea válida.
-                                    System.out.println("\nOPCION INVALIDA.\n");
-                                }  else {
-                                    radio.asignar((boton-1));
-                                    correcto = true;
-                                }
-
-                            } catch (InputMismatchException e){ //Si el usuario ingresa un valor no numerico, se ejecutará esto.
-                                System.out.println("\nPor favor ingrese una cantidad numerica.\n");
-                                scan.next();
-                            }
-                        }
+                        radio.asignar(0);
 
                     } else {
                         System.out.println("Necesita encender la radio primero para realizar esta accion");
                     }
-
 
                 }
                 else if (opcion == 6){
                     //Apagar la radio
                     if (radio.isON()) {
                         radio.apagar();
-                        System.out.println("Se ha apagado la radio");
                     }
                     else {
                         System.out.println("La radio ya estaba apagada...");
@@ -152,8 +98,6 @@ public class DriverMIPS {
                 scan.nextLine(); //Para que el programa continue funcionando
             }
         }
-       
 
     }
 }
-
